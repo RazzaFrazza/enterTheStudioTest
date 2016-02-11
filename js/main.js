86770819278ecdf97ae03eb0926d5ab7bce7c0d1
razzafrazza <networkingartist@gmail.com>
2016-02-11 07:35:54 -0800

    Prettified Code.

--- js/main.js
+++ js/main.js
@@ -1,26 +1,22 @@
-
 $(document).ready(function() {
-     
-     $('.toggle-btn').addClass('closed');
 
-     $('.closed').click(function() {
-          $('.main-nav').toggle();
-        
-         if($('.toggle-btn').hasClass('closed')) {
-              
-              $('.toggle-btn').removeClass('closed');
-              $('.toggle-btn').addClass('open');
-         
-          }
+    $('.toggle-btn').addClass('closed');
 
-          else if ($('.toggle-btn').hasClass('open')) {
-              
-              $('.toggle-btn').removeClass('open');
-              $('.toggle-btn').addClass('closed');
-         
-          }
-      });
+    $('.closed').click(function() {
+        $('.main-nav').toggle();
+
+        if ($('.toggle-btn').hasClass('closed')) {
+
+            $('.toggle-btn').removeClass('closed');
+            $('.toggle-btn').addClass('open');
+
+        } else if ($('.toggle-btn').hasClass('open')) {
+
+            $('.toggle-btn').removeClass('open');
+            $('.toggle-btn').addClass('closed');
+
+        }
+    });
 
 
-  });
-
+});
