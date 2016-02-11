178bce75b67ba874767efd1eac48d4cbbd016574
razzafrazza <networkingartist@gmail.com>
2016-02-11 07:21:24 -0800

    Added project files.

---
+++ src/js/main.js
@@ -0,0 +1,26 @@
+
+$(document).ready(function() {
+     
+     $('.toggle-btn').addClass('closed');
+
+     $('.closed').click(function() {
+          $('.main-nav').toggle();
+        
+         if($('.toggle-btn').hasClass('closed')) {
+              
+              $('.toggle-btn').removeClass('closed');
+              $('.toggle-btn').addClass('open');
+         
+          }
+
+          else if ($('.toggle-btn').hasClass('open')) {
+              
+              $('.toggle-btn').removeClass('open');
+              $('.toggle-btn').addClass('closed');
+         
+          }
+      });
+
+
+  });
+
